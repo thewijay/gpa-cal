1bb2ab08343d4ebdc1945c953457a002c61e4db0
Pubudu Wijesundara <neo17641@gmail.com>
2025-05-18 21:56:26 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <h1 className='text-yellow-500 font-semibold'>Hello World!</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
