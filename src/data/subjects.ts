261608659b91d4f0e040a51398ad5ad86808d49d
Pubudu Wijesundara <neo17641@gmail.com>
2025-06-22 09:41:35 +0530

    feat: update subjectData for Information Systems degree (all semesters)

--- src/data/subjects.ts
+++ src/data/subjects.ts
@@ -732,6 +732,352 @@ export const subjectData: FacultyMap = {
         electiveCreditsRequired: 0,
       },
     },
+    'Information Systems': {
+      'Semester 1': {
+        core: [
+          {
+            code: 'IS1101',
+            name: 'Fundamentals of Information Systems',
+            credits: 2,
+          },
+          {
+            code: 'IS1102',
+            name: 'Structured Programming Techniques',
+            credits: 2,
+          },
+          {
+            code: 'IS1103',
+            name: 'Structured Programming Practicum',
+            credits: 1,
+          },
+          {
+            code: 'IS1104',
+            name: 'Theories of Information Systems',
+            credits: 2,
+          },
+          { code: 'IS1105', name: 'Computer System Organization', credits: 2 },
+          {
+            code: 'IS1106',
+            name: 'Foundations of Web Technologies',
+            credits: 2,
+          },
+          {
+            code: 'IS1107',
+            name: 'Personal Productivity with Information Technology',
+            credits: 1,
+          },
+          { code: 'IS1108', name: 'Fundamentals of Mathematics', credits: 2 },
+          {
+            code: 'IS1109',
+            name: 'Statistics & Probability Theory',
+            credits: 2,
+          },
+        ],
+        electives: [],
+        electiveCreditsRequired: 0,
+      },
+      'Semester 2': {
+        core: [
+          { code: 'IS2101', name: 'Object Oriented Programming', credits: 2 },
+          {
+            code: 'IS2102',
+            name: 'Object Oriented Programming Practicum',
+            credits: 1,
+          },
+          { code: 'IS2103', name: 'Emerging IS Technologies', credits: 1 },
+          { code: 'IS2104', name: 'Database Systems', credits: 2 },
+          {
+            code: 'IS2105',
+            name: 'Database Management Systems Practicum',
+            credits: 1,
+          },
+          { code: 'IS2106', name: 'System Analysis & Design', credits: 1 },
+          { code: 'IS2107', name: 'Social & Professional Issues', credits: 1 },
+          { code: 'IS2108', name: 'Human Computer Interaction', credits: 2 },
+          {
+            code: 'IS2109',
+            name: 'Information Assurance & Security',
+            credits: 2,
+          },
+          {
+            code: 'IS21010',
+            name: 'Software Project Initiation & Planning',
+            credits: 1,
+          },
+          { code: 'IS21011', name: 'Advanced Mathematics', credits: 2 },
+        ],
+        electives: [],
+        electiveCreditsRequired: 0,
+      },
+      'Semester 3': {
+        core: [
+          {
+            code: 'IS3101',
+            name: 'Object Oriented Analysis & Design',
+            credits: 2,
+          },
+          {
+            code: 'IS3102',
+            name: 'Data Structures & Algorithms',
+            credits: 2,
+          },
+          { code: 'IS3103', name: 'IT Governance', credits: 2 },
+          { code: 'IS3104', name: 'Software Engineering', credits: 2 },
+          {
+            code: 'IS3105',
+            name: 'IS Risk Management',
+            credits: 2,
+          },
+          { code: 'IS3106', name: 'IS Sustainability', credits: 1 },
+          {
+            code: 'IS3107',
+            name: 'Management Information Systems',
+            credits: 2,
+          },
+          { code: 'IS3108', name: 'E-Business', credits: 1 },
+          {
+            code: 'IS3109',
+            name: 'Digital Innovation',
+            credits: 2,
+          },
+        ],
+        electives: [],
+        electiveCreditsRequired: 0,
+      },
+      'Semester 4': {
+        core: [
+          {
+            code: 'IS4101',
+            name: 'IT Auditing',
+            credits: 2,
+          },
+          {
+            code: 'IS4102',
+            name: 'Web Application Development',
+            credits: 2,
+          },
+          { code: 'IS4103', name: 'Operating Systems', credits: 2 },
+          {
+            code: 'IS4104',
+            name: 'System Administration and Maintenance',
+            credits: 2,
+          },
+          {
+            code: 'IS4105',
+            name: 'IT Procurement Management',
+            credits: 1,
+          },
+          { code: 'IS4106', name: 'Software Architecture', credits: 2 },
+          {
+            code: 'IS4107',
+            name: 'Professionalism & Ethics in Computing',
+            credits: 1,
+          },
+          { code: 'IS4108', name: 'IS Strategies', credits: 1 },
+          {
+            code: 'IS4109',
+            name: 'Agile Software Development',
+            credits: 2,
+          },
+          {
+            code: 'IS4110',
+            name: 'Capstone Project',
+            credits: 2,
+          },
+        ],
+        electives: [],
+        electiveCreditsRequired: 0,
+      },
+      'Semester 5': {
+        core: [
+          {
+            code: 'IS5101',
+            name: 'Entrepreneurship & Innovation',
+            credits: 1,
+          },
+          {
+            code: 'IS5102',
+            name: 'Enterprise Architecture',
+            credits: 1,
+          },
+          { code: 'IS5103', name: 'High Performance Computing', credits: 2 },
+          {
+            code: 'IS5104',
+            name: 'Software Process Management',
+            credits: 1,
+          },
+          {
+            code: 'IS5105',
+            name: 'Business Process Management',
+            credits: 2,
+          },
+          { code: 'IS5106', name: 'UI/UX Practicum', credits: 1 },
+          {
+            code: 'IS5107',
+            name: 'Project Management Practicum',
+            credits: 1,
+          },
+          { code: 'IS5108', name: 'Business Intelligence', credits: 2 },
+          {
+            code: 'IS5109',
+            name: 'IS Project for Community',
+            credits: 1,
+          },
+        ],
+        electives: [
+          {
+            code: 'IS5110',
+            name: 'Advanced Database Systems',
+            credits: 2,
+          },
+          {
+            code: 'IS5111',
+            name: 'Data Communication & Networks',
+            credits: 2,
+          },
+          {
+            code: 'IS5112',
+            name: 'Design Patterns & Anti-patterns',
+            credits: 2,
+          },
+          {
+            code: 'IS5113',
+            name: 'Software Quality Assurance',
+            credits: 2,
+          },
+          {
+            code: 'IS5114',
+            name: 'Data Mining & Analytics',
+            credits: 2,
+          },
+        ],
+        electiveCreditsRequired: 6,
+      },
+      'Semester 6': {
+        core: [
+          {
+            code: 'IS6101',
+            name: 'Industrial Training',
+            credits: 6,
+          },
+        ],
+        electives: [],
+        electiveCreditsRequired: 0,
+      },
+      'Semester 7': {
+        core: [
+          {
+            code: 'IS7101',
+            name: 'Research Methodologies',
+            credits: 2,
+          },
+          {
+            code: 'IS7102',
+            name: 'IT Law',
+            credits: 1,
+          },
+          { code: 'IS7103', name: 'Business Process Simulation', credits: 2 },
+          {
+            code: 'IS7104',
+            name: 'Enterprise Modelling Ontologies',
+            credits: 2,
+          },
+          {
+            code: 'IS7105',
+            name: 'Organizational Behavior & Management',
+            credits: 1,
+          },
+          { code: 'IS7106', name: 'Cloud Computing', credits: 2 },
+        ],
+        electives: [
+          {
+            code: 'IS7107',
+            name: 'Mobile Application Development',
+            credits: 1,
+          },
+          {
+            code: 'IS7108',
+            name: 'Web Service Technologies',
+            credits: 2,
+          },
+          {
+            code: 'IS7109',
+            name: 'Geographical Information Systems',
+            credits: 2,
+          },
+          {
+            code: 'IS7110',
+            name: 'Statistical Distribution & Inferences',
+            credits: 1,
+          },
+          {
+            code: 'IS7111',
+            name: 'Advanced Programming Practicum',
+            credits: 1,
+          },
+          {
+            code: 'IS7112',
+            name: 'Machine Learning',
+            credits: 2,
+          },
+        ],
+        electiveCreditsRequired: 4,
+      },
+      'Semester 8': {
+        core: [
+          {
+            code: 'IS8101',
+            name: 'Research Project in IS',
+            credits: 8,
+          },
+          {
+            code: 'IS8102',
+            name: 'Business/IT Alignment',
+            credits: 2,
+          },
+          { code: 'IS8103', name: 'Human Resource Management', credits: 2 },
+          {
+            code: 'IS8104',
+            name: 'Scientific Communication ',
+            credits: 1,
+          },
+          {
+            code: 'IS8105',
+            name: 'IS Economics',
+            credits: 2,
+          },
+          { code: 'IS8106', name: 'Computer System Security', credits: 2 },
+        ],
+        electives: [
+          {
+            code: 'IS8107',
+            name: 'Supply Chain Management',
+            credits: 2,
+          },
+          {
+            code: 'IS8108',
+            name: 'Advanced Computer Networks',
+            credits: 2,
+          },
+          {
+            code: 'IS8109',
+            name: 'Process Mining',
+            credits: 2,
+          },
+          {
+            code: 'IS8110',
+            name: 'Digital Business Model',
+            credits: 1,
+          },
+          {
+            code: 'IS8111',
+            name: 'Game Development',
+            credits: 2,
+          },
+        ],
+        electiveCreditsRequired: 4,
+      },
+    },
   },
 }
 
