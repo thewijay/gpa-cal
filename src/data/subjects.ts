9c5b88935715c4a818daf65816cabfcbd8592537
Pubudu Wijesundara <neo17641@gmail.com>
2025-07-01 19:55:36 +0530

    feat: update subjectData for Food Science and Technology degree (all semesters)

--- src/data/subjects.ts
+++ src/data/subjects.ts
@@ -1263,6 +1263,435 @@ export const subjectData: FacultyMap = {
       },
     },
   },
+  'Applied Sciences': {
+    'Food Science and Technology': {
+      'Semester 1': {
+        core: [
+          {
+            code: 'FST1101',
+            name: 'Introduction to Food Science and Technology',
+            credits: 2,
+          },
+          {
+            code: 'FST1102',
+            name: 'Food Biology and Practicum',
+            credits: 2,
+          },
+          {
+            code: 'FST1103',
+            name: 'General Chemistry for Food Science',
+            credits: 2,
+          },
+          {
+            code: 'FST1104',
+            name: 'Fundamentals of Organic Chemistry',
+            credits: 2,
+          },
+          {
+            code: 'FST1105',
+            name: 'Laboratory in Elementary Inorganic Chemistry',
+            credits: 1,
+          },
+          {
+            code: 'FST1106',
+            name: 'Introduction to Computer Fundamentals',
+            credits: 2,
+          },
+          {
+            code: 'FST1107',
+            name: 'Application of Computer Fundamentals',
+            credits: 1,
+          },
+          {
+            code: 'FST1108',
+            name: 'Principles of Mathematics',
+            credits: 2,
+          },
+          {
+            code: 'FST1109',
+            name: 'Production of Agricultural Raw Materials for Better Food Quality',
+            credits: 2,
+          },
+        ],
+        electives: [],
+        electiveCreditsRequired: 0,
+      },
+      'Semester 2': {
+        core: [
+          {
+            code: 'FST1201',
+            name: 'Fundamentals of Microbiology',
+            credits: 2,
+          },
+          {
+            code: 'FST1202',
+            name: 'Fundamentals of Biochemistry',
+            credits: 2,
+          },
+          {
+            code: 'FST1203',
+            name: 'Postharvest Technology',
+            credits: 2,
+          },
+          {
+            code: 'FST1204',
+            name: 'Postharvest Pest and Disease Management',
+            credits: 2,
+          },
+          {
+            code: 'FST1205',
+            name: 'Laboratory in Postharvest Handling of Food Sources',
+            credits: 1,
+          },
+          {
+            code: 'FST1206',
+            name: 'Fundamentals of Analytical Chemistry',
+            credits: 2,
+          },
+          {
+            code: 'FST1207',
+            name: 'Laboratory in Elementary Organic Chemistry',
+            credits: 1,
+          },
+          {
+            code: 'FST1208',
+            name: 'Fundamentals of Statistics',
+            credits: 2,
+          },
+          {
+            code: 'FST1209',
+            name: 'Fundamentals of Scientific and Technical Writing',
+            credits: 2,
+          },
+        ],
+        electives: [],
+        electiveCreditsRequired: 0,
+      },
+      'Semester 3': {
+        core: [
+          {
+            code: 'FST2101',
+            name: 'Advanced Biochemistry',
+            credits: 2,
+          },
+          {
+            code: 'FST2102',
+            name: 'Food Chemistry',
+            credits: 2,
+          },
+          {
+            code: 'FST2103',
+            name: 'Laboratory in Biochemistry and Food Chemistry',
+            credits: 1,
+          },
+          {
+            code: 'FST2104',
+            name: 'Principles of Human Nutrition',
+            credits: 2,
+          },
+          {
+            code: 'FST2105',
+            name: 'Food Microbiology',
+            credits: 2,
+          },
+          {
+            code: 'FST2106',
+            name: 'Food Preservation and Practicum',
+            credits: 2,
+          },
+          {
+            code: 'FST2107',
+            name: 'Management Process',
+            credits: 2,
+          },
+          {
+            code: 'FST2108',
+            name: 'Statistics for Experimental Analysis',
+            credits: 2,
+          },
+        ],
+        electives: [],
+        electiveCreditsRequired: 0,
+      },
+      'Semester 4': {
+        core: [
+          {
+            code: 'FST2201',
+            name: 'Biotechnology for Food Science',
+            credits: 2,
+          },
+          {
+            code: 'FST2202',
+            name: 'Laboratory in Food Microbiology and Biotechnology',
+            credits: 1,
+          },
+          {
+            code: 'FST2203',
+            name: 'Food Process Engineering I and Practicum',
+            credits: 2,
+          },
+          {
+            code: 'FST2204',
+            name: 'Livestock Production, Aquaculture Practices and Practicum',
+            credits: 2,
+          },
+          {
+            code: 'FST2205',
+            name: 'Applied Human Nutrition and Practicum',
+            credits: 2,
+          },
+          {
+            code: 'FST2206',
+            name: 'Food Toxicology',
+            credits: 1,
+          },
+          {
+            code: 'FST2207',
+            name: 'Food Quality Management',
+            credits: 2,
+          },
+          {
+            code: 'FST2208',
+            name: 'Statistical Methodology',
+            credits: 2,
+          },
+          {
+            code: 'FST2209',
+            name: 'Food Marketing',
+            credits: 1,
+          },
+        ],
+        electives: [],
+        electiveCreditsRequired: 0,
+      },
+      'Semester 5': {
+        core: [
+          {
+            code: 'FST3101',
+            name: 'Food Analysis and Practicum',
+            credits: 2,
+          },
+          {
+            code: 'FST3102',
+            name: 'Dairy Science',
+            credits: 2,
+          },
+          {
+            code: 'FST3103',
+            name: 'Food Process Engineering II and Practicum',
+            credits: 2,
+          },
+          {
+            code: 'FST3104',
+            name: 'Food Packaging',
+            credits: 2,
+          },
+          {
+            code: 'FST3105',
+            name: 'Food Regulations',
+            credits: 1,
+          },
+          {
+            code: 'FST3106',
+            name: 'Food Safety, Risk Analysis, Food Hygiene and Sanitation',
+            credits: 2,
+          },
+          {
+            code: 'FST3107',
+            name: 'Food Product Development',
+            credits: 1,
+          },
+          {
+            code: 'FST3108',
+            name: 'Environmental Sustainability in Food Industries',
+            credits: 2,
+          },
+          {
+            code: 'FST3109',
+            name: 'Human Resource Management',
+            credits: 1,
+          },
+          {
+            code: 'FST3110',
+            name: 'Research Methodology and Scientific Communication',
+            credits: 2,
+          },
+        ],
+        electives: [],
+        electiveCreditsRequired: 0,
+      },
+      'Semester 6': {
+        core: [
+          {
+            code: 'FST3201',
+            name: 'Aquatic Food Processing Technology',
+            credits: 2,
+          },
+          {
+            code: 'FST3202',
+            name: 'Dairy Processing Technology',
+            credits: 2,
+          },
+          {
+            code: 'FST3203',
+            name: 'Laboratory in Dairy Science and Dairy Processing Technology',
+            credits: 1,
+          },
+          {
+            code: 'FST3204',
+            name: 'Beverage Processing Technology',
+            credits: 1,
+          },
+          {
+            code: 'FST3205',
+            name: 'Sensory Evaluation of Foods and Practicum',
+            credits: 2,
+          },
+          {
+            code: 'FST3206',
+            name: 'Functional Foods and Nutraceuticals',
+            credits: 1,
+          },
+          {
+            code: 'FST3207',
+            name: 'Process Control and Automation in Food Industry',
+            credits: 1,
+          },
+          {
+            code: 'FST3208',
+            name: 'Seminars in Trends and Current Issues in Food Science and Technology',
+            credits: 1,
+          },
+        ],
+        electives: [
+          {
+            code: 'FST3209',
+            name: 'Instrumental Techniques in Food Science',
+            credits: 1,
+          },
+          {
+            code: 'FST3210',
+            name: 'Food Plant Layout and Operations',
+            credits: 2,
+          },
+          {
+            code: 'FST3211',
+            name: 'Statistics for Research',
+            credits: 1,
+          },
+          {
+            code: 'FST3212',
+            name: 'Nutritional Aspects of Chronic Diseases',
+            credits: 1,
+          },
+        ],
+        electiveCreditsRequired: 3,
+      },
+      'Semester 7': {
+        core: [
+          {
+            code: 'FST4101',
+            name: 'Integrated Project in Food Science and Technology',
+            credits: 2,
+          },
+          {
+            code: 'FST4102',
+            name: 'Chemistry and Technology of Cereals',
+            credits: 2,
+          },
+          {
+            code: 'FST4103',
+            name: 'Chemistry and Technology of Fats and Oils',
+            credits: 2,
+          },
+          {
+            code: 'FST4104',
+            name: 'Spice, Root and Tuber Processing Technology',
+            credits: 1,
+          },
+          {
+            code: 'FST4105',
+            name: 'Pulse and Edible Nut Processing Technology',
+            credits: 1,
+          },
+          {
+            code: 'FST4106',
+            name: 'Sugar and Confectionery Processing Technology',
+            credits: 2,
+          },
+          {
+            code: 'FST4107',
+            name: 'Fruit and Vegetable Processing Technology',
+            credits: 1,
+          },
+          {
+            code: 'FST4108',
+            name: 'Laboratory in Food Processing Technology (Cereals, Spices, Roots, Tubers, Pulses, Confectionery, Fruits, Vegetables)',
+            credits: 1,
+          },
+          {
+            code: 'FST4109',
+            name: 'Meat and Egg Processing Technology',
+            credits: 2,
+          },
+          {
+            code: 'FST4110',
+            name: 'Laboratory in Aquatic Food, Meat and Egg Processing Technology',
+            credits: 1,
+          },
+          {
+            code: 'FST4111',
+            name: 'Advanced Food Quality Management',
+            credits: 2,
+          },
+          {
+            code: 'FST4112',
+            name: 'Entrepreneurship in Food Technology',
+            credits: 2,
+          },
+        ],
+        electives: [
+          {
+            code: 'FST4113',
+            name: 'Nanotechnology and its Applications in Food',
+            credits: 2,
+          },
+          {
+            code: 'FST4114',
+            name: 'Technology and Innovation Management',
+            credits: 1,
+          },
+          {
+            code: 'FST4115',
+            name: 'Food Culture and Traditional Foods',
+            credits: 1,
+          },
+          {
+            code: 'FST4116',
+            name: 'Modern Food Supply and Distribution Systems and Sustainability',
+            credits: 1,
+          },
+          {
+            code: 'FST4117',
+            name: 'Data Science and Informatics Applications in Food Science',
+            credits: 1,
+          },
+        ],
+        electiveCreditsRequired: 3,
+      },
+      'Semester 8': {
+        core: [
+          {
+            code: 'FST4201',
+            name: 'Research Project in Food Science and Technology',
+            credits: 8,
+          },
+        ],
+        electives: [],
+        electiveCreditsRequired: 0,
+      },
+    },
+  },
 }
 
 export type { Subject, SemesterSubjects, SemesterMap, DegreeMap, FacultyMap }
