553bf7f8bd4af6b035df6b9bc91addef22e679ad
Tharuka Senevirathne <tharukasenevirathne256@gmail.com>
2025-08-13 20:41:38 +0530

    feat(data): add Agricultural Sciences faculty with FBM degree

--- src/data/subjects/index.ts
+++ src/data/subjects/index.ts
@@ -2,11 +2,13 @@ import type { FacultyMap } from '../types'
 import { computing } from './computing'
 import { appliedSciences } from './appliedSciences'
 import { managementStudies } from './managementStudies'
+import { agriculturalSciences } from './agriculturalSciences'
 
 export const subjectData: FacultyMap = {
   Computing: computing,
   'Applied Sciences': appliedSciences,
   'Management Studies': managementStudies,
+  'Agricultural Sciences': agriculturalSciences,
 }
 
 // Re-export types for convenience
